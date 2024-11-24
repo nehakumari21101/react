80705e1f2d26a94b7c6a93b38cdfec4db573a55b
Neha kumari <kumarineha122162@gmail.com>
2024-11-24 20:08:10 +0530

    Add Restaurant category items

---
+++ src/components/ItemList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ItemList = ({items}) =>{
+    console.log(items)
+    return(
+        <div>
+            <h3>ItemList</h3>
+        </div>
+    )
+}
+
+export default ItemList;
