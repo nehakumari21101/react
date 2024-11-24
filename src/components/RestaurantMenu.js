4b8404c71f2d1dfcd44369d589b24641d611e0e2
Neha kumari <kumarineha122162@gmail.com>
2024-11-24 20:09:16 +0530

    Update features

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
@@ -23,7 +24,7 @@ const RestaurantMenu = () => {
     // console.log(json.data);
     setResInfo(json.data);
 
-    console.log(json.data);
+    // console.log(json.data);
 
     //     const { name, cuisines, costForTwoMessage } =
     // resInfo?.cards[2].card?.card?.info;
@@ -37,18 +38,27 @@ const RestaurantMenu = () => {
   const { itemCards } =
     resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card;
 
+    // console.log(resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards);
+
+    // console.log(resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2]?.card?.card?.["@type"]
+    // );
+
+    const categories =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.["card"]?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+    // console.log(categories);
+
   return (
-    <div className="border border-solid border-red">
-      <h2>{name}</h2>
-      <h3>{cuisines.join(", ")}</h3>
-      <p>{costForTwoMessage}</p>
-      <ul style={{ margin: "10px" }}>
-        {itemCards.map((item) => (
-          <li key={item.card.info.id}>
-            {item.card.info.name} - Rs.{item.card.info.price / 100}
-          </li>
-        ))}
-      </ul>
+    <div className="text-center border border-solid border-red">
+      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">{cuisines.join(", ")} - {costForTwoMessage}</p>
+      {categories.map((category, index) => (
+        // controlled component
+        <RestaurantCategory data={category?.card?.card} key={index}/>
+      ))}
     </div>
   );
 };
